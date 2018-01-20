69e0b85694780f860f974e33aa2f818b2cc15dc2
CliffJumper <1405566+CliffJumper@users.noreply.github.com>
2018-01-20 08:01:15 -0500

    Added test of the algo when run standalone

--- binary_search.py
+++ binary_search.py
@@ -14,3 +14,31 @@ def recursive_binsearch(A,k) :
     else:
         return binsearch(A[size//2:],k)
 
+
+if __name__ == '__main__':
+    # Test the fun
+    import random
+    A = [4,5,7,54,234,73,5,8,35,8,35,8,3457,3835,8,358,]
+    B = random.sample(range(100),25)
+    print(A)
+    print("Searching A for:", 5)
+    print("\t",recursive_binsearch(A,5))
+    
+    print("Searching A for:", 3567)
+    print("\t",recursive_binsearch(A,5))
+    
+    print("Searching A for:", 54)
+    print("\t",recursive_binsearch(A,5))
+        
+    print(B)
+    print("Searching B for:", 5)
+    print("\t",recursive_binsearch(B,5))
+       
+    print("Searching B for:", 57)
+    print("\t",recursive_binsearch(B,57))
+    
+    print("Searching B for:", 81)
+    print("\t",recursive_binsearch(B,81))
+    
+    
+
