4eae32e28df69dc69498df3c37b98e3a01832930
Cliff Free <1405566+CliffJumper@users.noreply.github.com>
2018-04-06 19:19:02 -0400

    Added example argparse usage

---
+++ argparse_example.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+# Setup Parser and args
+
+parser = argparse.ArgumentParser()
+
+# Add positional Arg
+parser.add_argument("square", type=int, help="Number which is to be squared")
+
+# Optional Arg
+parser.add_argument("-v","--verbosity", help="Increase verbosity of output")
+
+# Required Arg
+parser.add_argument("-a","--account",help="Account Number", required=True)
+
+# Parse Args
+args = parser.parse_args()
+
+
