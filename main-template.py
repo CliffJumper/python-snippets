303c681f681da0e3c6ec639de226392ea4334a25
Cliff Free <nucular.limozeen@gmail.com>
2019-01-05 18:18:45 -0500

    Added argparse and class boilerplate

--- main-template.py
+++ main-template.py
@@ -1,5 +1,34 @@
+# imports
+import argparse
+
+class MyClass:
+
+    def __init__(self, string):
+        self.mystring = string
+    
+    def f(self):
+        return self.mystring
 
 
+###############################################################################
 # Main entry (use for testing what's in this file if it's not the app's entry)
+###############################################################################
 if __name__ == '__main__':
-    print("Hello, world")
+
+    parser = argparse.ArgumentParser()
+
+    ###########################################################################
+    # Examples for argparse usage; change these at the program needs
+    ###########################################################################
+    # Add positional Arg
+    #parser.add_argument("square", type=int, help="Number which is to be squared")
+    # Optional Arg
+    parser.add_argument("-v","--verbosity", help="Increase verbosity of output")
+    # Required Arg
+    parser.add_argument("-s","--string",help="String to print", required=True)
+    # Parse Args
+    args = parser.parse_args()
+
+    x = MyClass(args.string)
+
+    print(x.f())
